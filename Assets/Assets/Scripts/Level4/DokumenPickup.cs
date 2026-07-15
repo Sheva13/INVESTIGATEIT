5b34256ace154d185e39abf9d402faa847a1ccf8
rafli <raflyfirmansyah987@gmail.com>
2026-07-15 12:06:35 +0700

    pindah ke dialog

--- Assets/Assets/Scripts/Level4/DokumenPickup.cs
+++ Assets/Assets/Scripts/Level4/DokumenPickup.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using TMPro;
 using Level4;
 
 public class DokumenPickup : HoldInteraction
 {
     [Header("Dokumen")]
-    public Color lockedTint = new Color(0.7f, 0.7f, 0.7f, 1f);
     public Color normalTint = Color.white;
+
+    [Header("Popup")]
+    public TextMeshProUGUI popupPrompt;
+    public string popupMessage = "Tekan [M] untuk ambil dokumen";
 
     private SpriteRenderer spriteRenderer;
 
@@ -13,32 +17,64 @@ public class DokumenPickup : HoldInteraction
     {
         base.Awake();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        holdPrompt = "Tahan E untuk mengambil dokumen";
-        lockedPrompt = "Kumpulkan semua kaleng terlebih dahulu";
+        holdPrompt = "";
+        lockedPrompt = "";
     }
 
     protected override bool CanInteract()
     {
-        if (isComplete) return false;
-        if (!Level4State.hasAllCans) return false;
-        return base.CanInteract();
+        return false;
     }
 
     protected override void Update()
     {
-        base.Update();
-        if (spriteRenderer != null)
+        if (isComplete) return;
+
+        if (spriteRenderer != null && !isComplete)
+            spriteRenderer.color = normalTint;
+
+        if (isInRange && !isComplete)
         {
-            if (Level4State.hasAllCans && !isComplete)
-                spriteRenderer.color = normalTint;
-            else if (!isComplete)
-                spriteRenderer.color = lockedTint;
+            if (popupPrompt != null)
+            {
+                popupPrompt.gameObject.SetActive(true);
+                popupPrompt.text = popupMessage;
+            }
+
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                isComplete = true;
+                if (popupPrompt != null)
+                    popupPrompt.gameObject.SetActive(false);
+                if (gm != null)
+                    gm.CollectDocument();
+            }
         }
+        else
+        {
+            if (popupPrompt != null)
+                popupPrompt.gameObject.SetActive(false);
+        }
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController2 pc = other.GetComponentInParent<PlayerController2>();
+        if (pc == null) return;
+        isInRange = true;
+        playerTransform = pc.transform;
+    }
+
+    protected override void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponentInParent<PlayerController2>() == null) return;
+        isInRange = false;
+        playerTransform = null;
+        holdTimer = 0f;
+        isHolding = false;
     }
 
     protected override void OnInteractionComplete()
     {
-        if (gm != null)
-            gm.CollectDocument();
     }
 }
